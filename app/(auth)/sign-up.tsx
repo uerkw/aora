f8c7d86cd25103b77d5791b11db0d13f1c540fb3
uerkw <38047633+uerkw@users.noreply.github.com>
2024-09-09 01:34:42 -0400

    Add appwrite, environment vars loading, and sign up flow

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,12 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -16,7 +17,25 @@ const SignUp = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submitForm = () => {};
+  const submitForm = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert("Error", "Please fill all the fields");
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      // set it to global state using context...
+
+      router.replace("/home");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
---
+++ lib/appwrite.ts
@@ -0,0 +1,76 @@
+import "react-native-url-polyfill/auto";
+
+export const appwriteConfig = {
+  projectId: process.env["EXPO_PUBLIC_APPWRITE_PROJECT_ID"],
+  databaseId: process.env["EXPO_PUBLIC_APPWRITE_DATABASE_ID"],
+  userCollectionId: process.env["EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID"],
+  videoCollectionId: process.env["EXPO_PUBLIC_APPWRITE_VIDEO_COLLECTION_ID"],
+  storageId: process.env["EXPO_PUBLIC_APPWRITE_STORAGE_ID"],
+  endpoint: process.env["EXPO_PUBLIC_APPWRITE_ENDPOINT"],
+  platform: process.env["EXPO_PUBLIC_APPWRITE_PLATFORM"],
+};
+
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
+// Init your React Native SDK
+const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint!) // Your Appwrite Endpoint
+  .setProject(appwriteConfig.projectId!) // Your project ID
+  .setPlatform(appwriteConfig.platform!); // Your application ID or bundle ID.
+
+const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+export const createUser = async (
+  email: string,
+  password: string,
+  username: string
+) => {
+  // Register User
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) throw Error("User not created from appwrite");
+
+    const avatarUrl = avatars.getInitials(username);
+
+    await signInUser(email, password);
+
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId!,
+      appwriteConfig.userCollectionId!,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email,
+        username,
+        avatar: avatarUrl,
+      }
+    );
+
+    if (!newUser) throw Error("User not created in database");
+
+    return newUser;
+  } catch (error: string | unknown) {
+    console.log(error);
+    throw new Error("User not created");
+  }
+};
+
+export async function signInUser(email: string, password: string) {
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+    if (!session) throw Error("User not signed in");
+    return session;
+  } catch (error: string | unknown) {
+    console.log(error);
+    throw new Error("User not signed in");
+  }
+}
